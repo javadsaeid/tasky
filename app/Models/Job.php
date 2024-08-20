ff00496d1c7c5bf35ade494b84549954f599b011
javad <javadsaeid8@gmail.com>
2024-08-20 09:10:56 +0330

    add relation for Employer and Job

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,4 +9,9 @@ class Job extends Model
 {
     use HasFactory;
     protected $table = 'job_listings';
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,8 @@ class JobFactory extends Factory
     {
         return [
             'title' => fake()->jobTitle,
-            'salary' => '$5000 USD'
+            'salary' => '$5000 USD',
+            'employer_id' => Employer::factory()
         ];
     }
 }
